ad2868bf7aa6c322317328c6175a8762b04fa837
marmaryia <marozavamaryia@gmail.com>
2025-07-05 17:14:55 +0100

    make sure the timestamp is treated as utc

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,4 +1,8 @@
 export function formatIsoTimestamp(timestamp: string, dateOnly: boolean) {
+  if (timestamp.toUpperCase().charAt(-1) !== "Z") {
+    timestamp += "Z";
+  }
+
   const date = new Date(timestamp);
   if (dateOnly) {
     return date.toLocaleDateString("en-GB");
