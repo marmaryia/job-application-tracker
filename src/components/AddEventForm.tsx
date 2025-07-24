768a082a8476e94c69e106f7b64c751c602f9e95
marmaryia <marozavamaryia@gmail.com>
2025-07-24 15:41:11 +0100

    make form to gather new event data

--- src/components/AddEventForm.tsx
+++ src/components/AddEventForm.tsx
@@ -1,5 +1,53 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/userContext";
+import { useParams } from "react-router-dom";
+import { handleDataEntry } from "../utils/dataEntry";
+
 function AddEventForm() {
-  return <div>AddEventForm</div>;
+  const { loggedInUser } = useContext(UserContext);
+  const application_id = Number(useParams().application_id);
+  const [eventData, setEventData] = useState({
+    user_id: loggedInUser!.id,
+    application_id: application_id,
+    title: "",
+    date: new Date(Date.now()).toISOString(),
+    notes: "",
+  });
+
+  console.log(eventData);
+
+  return (
+    <form action="submit">
+      <h3 className="vertical-timeline-element-title">New event</h3>
+      <label htmlFor="event-title">Event:</label> <br />
+      <input
+        required
+        type="text"
+        id="event-title"
+        onChange={(e) => handleDataEntry("title", e.target.value, setEventData)}
+        value={eventData.title}
+      />
+      <br />
+      <label htmlFor="event-notes">Notes:</label> <br />
+      <input
+        type="text"
+        id="event-notes"
+        onChange={(e) => handleDataEntry("notes", e.target.value, setEventData)}
+        value={eventData.notes}
+      />
+      <br />
+      <label htmlFor="event-date">Date:</label> <br />
+      <input
+        required
+        type="date"
+        id="event-notes"
+        value={eventData.date.split("T")[0]}
+        max={new Date(Date.now()).toISOString().split("T")[0]}
+        onChange={(e) => handleDataEntry("date", e.target.value, setEventData)}
+      />
+      <br />
+    </form>
+  );
 }
 
 export default AddEventForm;
