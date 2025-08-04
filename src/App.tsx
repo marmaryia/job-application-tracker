5f9adf76f59d0b61c7b077faaafc652fe8006e15
marmaryia <marozavamaryia@gmail.com>
2025-08-04 13:29:44 +0100

    add home page that redirects depending on authentication status

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./styles/PopupStyles.css";
-import "./styles/ApplicationPageStyles.css"
+import "./styles/ApplicationPageStyles.css";
 
 import Header from "./components/Header";
 import ApplicationsList from "./components/ApplicationsListPage";
 import ApplicationPage from "./components/ApplicationPage";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <main>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/applications" element={<ApplicationsList />} />
